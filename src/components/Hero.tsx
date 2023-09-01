19f0f6b80d414cfc7508e7c468195485413a6942
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-01 11:33:21 +0530

    about

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,55 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
 import logo from '../assets/logomecocnf.svg'
+import iee from '../assets/IEEE MEC SB LOGO WHITE T 1.svg'
+import computersociety from '../assets/CSLogo-200x70 1.svg'
 type Props = {}
 
 const Hero = (props: Props) => {
   return (
     <section className=" ">
-      <div className="py-8 px-4 mx-auto h-[100vh] max-w-screen-xl text-center lg:py-16 lg:px-12">
+      <div className="py-8 px-4 mx-auto h-[100vh] max-w-screen-xl text-center lg:py-16 lg:px-12 flex flex-col justify-evenly">
+          <div className='flex flex-row justify-between gap-2 max-h-   '>
+        <Image src={computersociety} alt="logo" className="h-full mt-2" />
+       <Image src={iee} alt="logo" className="h-full mt-2" />
+       </div>
    
        <div className='flex flex-row justify-center gap-2 max-h-   '>
         <Image src={logo} alt="logo" className="h-full mt-2" />
-         <h1 className="mb-4 text-[4rem] md:text-[6rem] herotext dark:text-white">
+         <h1 className=" text-[4rem] md:text-[6rem] herotext dark:text-white my-auto">
           MEC CONF
         </h1>
        </div>
-       
-        <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-          <a
-            href="#"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-          >
-            Learn more
-            <svg
-              className="ml-2 -mr-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a
-            href="#"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-          >
-            <svg
-              className="mr-2 -ml-1 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-            </svg>
-            Watch video
-          </a>
-        </div>
-        <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
+      
+        <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36 uppercase">
+          October 10 <br/>
+          Govt. Model Engineering College, Thrikkakara
          
           
         </div>
