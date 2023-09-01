cbb7dc9909ddd334e711adac632087d3f43824ce
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-02 00:40:41 +0530

    huhh

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,28 @@
 import Image from 'next/image'
+ import dynamic from 'next/dynamic'
 import { Inter } from 'next/font/google'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
-import CountdownTimer from '@/components/Countdown'
+
 import Navbar from '@/components/Navbar'
 import bgmain from'../assets/bgmain.svg'
 
 const inter = Inter({ subsets: ['latin'] })
+ 
+const NoSSR = dynamic(() => import('../components/Countdown'), { ssr: false })
 
 export default function Home() {
+ 
+
   return (
     <main
+      
     
       // className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <Navbar/>
       <Hero/>
-       <CountdownTimer targetDate={new Date('September 23, 2023 09:30:00').getTime()}/>
+       <NoSSR targetDate={new Date('September 23, 2023 09:30:00').getTime()}/>
       <About/>
      
      
