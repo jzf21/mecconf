e939cfc3016ff32697fccb08b5a3f312c1f7fea3
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-03 13:25:21 +0530

    responsive?

--- src/components/contact-card/Contactcard.jsx
+++ src/components/contact-card/Contactcard.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import styles from './Contactcard.module.css'
+import Image from 'next/image';
 function Contactcard(props) {
   return (
     <>
-    <div className={styles.contactcard}>
-        
+      <div className="flex flex-col justify-center items-center max-w-[300px] rounded-xl p-4 bg-[#cccef4]">
+        <img
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+          alt="Contact us self-center mx-auto"
+          className='w-[90%]'
+          // width={300}
+          // height={300}
+        />
+
+        <div className='flex flex-col justify-center text-center gap-1'>
+          <p className='text-blue-500 text-lg'>{props.name}</p>
+          <p className='text-black text-sm'>{props.designation}</p>
+          <div className='flex flex-row gap-4'>
+
+          </div>
+        </div>
+        {/*         
         <h3 className={styles.contactname}>{props.name}</h3>
         <h3 className={styles.contactemail}>{props.email}</h3>
-        <h3 className={styles.contactmobile}>{props.mobile}</h3>
-    </div>
+        <h3 className={styles.contactmobile}>{props.mobile}</h3> */}
+      </div>
     </>
-  )
+  );
 }
 
 export default Contactcard
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -4,35 +4,37 @@ import styles from './contact.module.css'
 const contact = () => {
   return (
     <div>
-        <h2 className={styles.contacttitle}>Contact</h2>
-        <div className={styles.contactholder}>
-            <ContactCard
-                name='JOHN DOE'
-                email="something@gmail.com"
-                mobile="1234567890"
-            />
-            <ContactCard
-                name='JOHN DOE'
-                email="something@gmail.com"
-                mobile="1234567890"
-            />
-            
-        </div>
-        <div className={styles.contactholder}>
-            <ContactCard
-                name='JOHN DOE'
-                email="something@gmail.com"
-                mobile="1234567890"
-            />
-            <ContactCard
-                name='JOHN DOE'
-                email="something@gmail.com"
-                mobile="1234567890"
-            />
-            
-        </div>
+      <h2 className={styles.contacttitle}>Contact</h2>
+      <div className={styles.contactholder}>
+        <ContactCard
+          name="JOHN DOE"
+          email="something@gmail.com"
+          mobile="1234567890"
+          designation="CEO"
+        />
+        <ContactCard
+          name="JOHN DOE"
+          email="something@gmail.com"
+          mobile="1234567890"
+          designation="CEO"
+        />
+      </div>
+      <div className={styles.contactholder}>
+        <ContactCard
+          name="JOHN DOE"
+          email="something@gmail.com"
+          mobile="1234567890"
+          designation="CEO"
+        />
+        <ContactCard
+          name="JOHN DOE"
+          email="something@gmail.com"
+          mobile="1234567890"
+          designation="CEO"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default contact
