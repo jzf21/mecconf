8dba3e20941f3b40fbc811d7c7fd11e4a0bc0bab
Joel Roshan <joeljosephroshan@gmail.com>
2023-09-03 12:19:16 +0530

    more about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,6 +10,21 @@ const About = (props: Props) => {
         <p className='text-white font-normal font-sans'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vitae, esse voluptatem perferendis explicabo in enim laborum repudiandae numquam excepturi et iusto accusamus vel dignissimos magnam quas, architecto quos rem? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid molestias et quibusdam dignissimos voluptatem unde, odio neque cum. Accusamus ratione corporis, similique quam distinctio ullam rerum! Sapiente, fugiat! Distinctio, libero?</p>
       
       </div>
+      <div className="py-8 px-4 mx-auto  max-w-screen-xl text-center lg:py-16 lg:px-12 flex flex-col justify-evenly gap-4">
+        <h2 className='text-white uppercase font-semibold text-[2rem] md:text-[2.3rem]'>About <span className='text-blue-500'>IEEE MEC SB</span></h2>
+        <p className='text-white font-normal font-sans'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vitae, esse voluptatem perferendis explicabo in enim laborum repudiandae numquam excepturi et iusto accusamus vel dignissimos magnam quas, architecto quos rem? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid molestias et quibusdam dignissimos voluptatem unde, odio neque cum. Accusamus ratione corporis, similique quam distinctio ullam rerum! Sapiente, fugiat! Distinctio, libero?</p>
+      
+      </div>
+      <div className="py-8 px-4 mx-auto  max-w-screen-xl text-center lg:py-16 lg:px-12 flex flex-col justify-evenly gap-4">
+        <h2 className='text-white uppercase font-semibold text-[2rem] md:text-[2.3rem]'>About <span className='text-blue-500'>FOSS MEC</span></h2>
+        <p className='text-white font-normal font-sans'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vitae, esse voluptatem perferendis explicabo in enim laborum repudiandae numquam excepturi et iusto accusamus vel dignissimos magnam quas, architecto quos rem? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid molestias et quibusdam dignissimos voluptatem unde, odio neque cum. Accusamus ratione corporis, similique quam distinctio ullam rerum! Sapiente, fugiat! Distinctio, libero?</p>
+      
+      </div>
+      <div className="py-8 px-4 mx-auto  max-w-screen-xl text-center lg:py-16 lg:px-12 flex flex-col justify-evenly gap-4">
+        <h2 className='text-white uppercase font-semibold text-[2rem] md:text-[2.3rem]'>About <span className='text-blue-500'>IEEE CS</span></h2>
+        <p className='text-white font-normal font-sans'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vitae, esse voluptatem perferendis explicabo in enim laborum repudiandae numquam excepturi et iusto accusamus vel dignissimos magnam quas, architecto quos rem? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid molestias et quibusdam dignissimos voluptatem unde, odio neque cum. Accusamus ratione corporis, similique quam distinctio ullam rerum! Sapiente, fugiat! Distinctio, libero?</p>
+      
+      </div>
     </section>
   )
 }
