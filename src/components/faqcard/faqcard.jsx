934050bceb0536edd2656bd638068acd89ed3929
Joel Roshan <joeljosephroshan@gmail.com>
2023-09-03 07:59:12 +0530

    close button

--- src/components/faqcard/faqcard.jsx
+++ src/components/faqcard/faqcard.jsx
@@ -4,20 +4,29 @@ import { useState } from 'react'
 
 function Faqcard(props) {
   const [open, setOpen] = useState(false)
+  
   const toggleOpen = () => {
     setOpen(!open);
   };
+  
+  
   return (
     <div className={styles.faqcardbody}>
       <div className={styles.faqcardcontent}>
       <p className={styles.faqcardquestion}>{props.question}</p>
       
-      {open && <p className={styles.faqcardanswer}>{props.answer}</p>}
+      {open && <p className={styles.faqcardanswer}>{props.answer}</p>
+      
+      
+      }
       
     </div>
-    <div className={styles.faqcardopen} onClick={toggleOpen}>
+    {!open&&<div className={styles.faqcardopen} onClick={toggleOpen}>
         
-    </div>
+    </div>}
+   {open && <button className={styles.faqcardclose} onClick={toggleOpen}>
+                X
+              </button>}
     </div>
   )
 }
