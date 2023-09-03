f3765be225f1fea535496533c29ded4537e0163e
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-03 17:09:28 +0530

    fasda

--- src/components/contact-card/Contactcard.jsx
+++ src/components/contact-card/Contactcard.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
-import styles from './Contactcard.module.css'
-import Image from 'next/image';
+import React from "react";
+import styles from "./Contactcard.module.css";
+import Image from "next/image";
+import phone from "../../assets/phone.svg";
+import email from '../../assets/ic_round-mail.svg'
+import linkedin from '../../assets/bi_linkedin.svg'
+import Link from "next/link";
+
 function Contactcard(props) {
   return (
     <>
@@ -8,16 +13,24 @@ function Contactcard(props) {
         <img
           src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
           alt="Contact us self-center mx-auto"
-          className='w-[90%]'
+          className="w-[90%]"
           // width={300}
           // height={300}
         />
 
-        <div className='flex flex-col justify-center text-center gap-1'>
-          <p className='text-blue-500 text-lg'>{props.name}</p>
-          <p className='text-black text-sm'>{props.designation}</p>
-          <div className='flex flex-row gap-4'>
-
+        <div className="flex flex-col justify-center text-center gap-1">
+          <p className="text-blue-500 text-lg">{props.name}</p>
+          <p className="text-black text-sm">{props.designation}</p>
+          <div className="flex flex-row gap-8 mt-2">
+            <Link href={props.mobile}>
+              <Image src={phone} alt="phone" className="w-6 h-6" />
+            </Link>
+            <Link href={props.mobile}>
+              <Image src={email} alt="email" className="w-6 h-6" />
+            </Link>
+            <Link href={props.mobile}>
+              <Image src={linkedin} alt="linkedin" className="w-6 h-6" />
+            </Link>
           </div>
         </div>
         {/*         
@@ -29,4 +42,4 @@ function Contactcard(props) {
   );
 }
 
-export default Contactcard
+export default Contactcard;
