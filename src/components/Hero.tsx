b99b53ed3ae15af1d78c90917639805f0e1bce9d
Joel Roshan <joeljosephroshan@gmail.com>
2023-09-04 13:29:58 +0530

    genius

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,9 +34,13 @@ const Hero = (props: Props) => {
             <Image src={date} alt="logo" className="h-6 w-6 sm:h-8 sm:w-8 md:h-10 md:w-10 " />
             <p className='text-white text-xs sm:text-lg' >September 23,24</p>
           </div>
-            <div className='flex flex-row gap-4 justify-center items-center'>
+            <div className='hidden min-[420px]:flex flex-row gap-4  justify-center items-center'>
             <Image src={location} alt="logo" className="h-6 w-6 sm:h-8 sm:w-8 md:h-10 md:w-10 " />
-            <p className='text-white text-xs sm:text-lg'>  Govt. Model Engineering College, Thrikkakara</p>
+            <p className='text-white text-xs  sm:text-lg'>  Govt. Model Engineering College,Thrikkakara</p>
+          </div>
+          <div className='flex min-[420px]:hidden flex-row gap-4  justify-center items-center'>
+            <Image src={location} alt="logo" className="h-6 w-6 sm:h-8 sm:w-8 md:h-10 md:w-10 " />
+            <p className='text-white text-xs  sm:text-lg'>  Govt. Model Engineering College,<br/>Thrikkakara</p>
           </div>
         
          
